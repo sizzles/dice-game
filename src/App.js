ee80e81e7281494aaba459a11b1f8a11bd61ac6e
Alex <Alex@>
2021-11-01 22:45:30 +0000

    Update app tab title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Game  from './components/game';
-
+import React, { useEffect } from 'react';
 function App() {
+
+  useEffect(() => { document.title = "Dice Game" }, [])
+
   return (
     <div className="App">
       <Game></Game>
